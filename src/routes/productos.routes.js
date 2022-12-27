c57c1a6f9b91086cc274a00846b00782e21a8195
Mauro Asselborn <mauasselborn@gmail.com>
2022-12-27 15:41:44 -0300

    Mini changes

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,19 +1,19 @@
-import { Router } from 'express';
+import { Router } from 'express'
 import {
-	listarProductos,
-	listarProducto,
-	crearProducto,
-	actualizarProducto,
-	borrarProducto,
-} from '../controllers/productos.controller.js';
+    listarProductos,
+    listarProducto,
+    crearProducto,
+    actualizarProducto,
+    borrarProducto,
+} from '../controllers/productos.controller.js'
 
-const router = Router();
+const router = Router()
 
 // Peticiones http
-router.get('/productos', listarProductos);
-router.get('/productos/:id', listarProducto);
-router.post('/productos', crearProducto);
-router.put('/productos/:id', actualizarProducto);
-router.delete('/productos/:id', borrarProducto);
+router.get('/productos', listarProductos)
+router.get('/productos/:id', listarProducto)
+router.post('/productos', crearProducto)
+router.put('/productos/:id', actualizarProducto)
+router.delete('/productos/:id', borrarProducto)
 
-export default router;
+export default router
