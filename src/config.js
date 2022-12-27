0f60a0f91e7b828c188249b79765d03f75488234
Mauro Asselborn <mauasselborn@gmail.com>
2022-12-27 20:16:03 -0300

    tratando de resolver la cagada de eze

--- src/config.js
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv'
-config()
-
-export const DB_PORT = process.env.DB_PORT
-export const PORT = process.env.PORT
-export const DB_DATABASE = process.env.DB_DATABASE
-export const DB_USERNAME = process.env.DB_USERNAME
-export const DB_PASSWORD = process.env.DB_PASSWORD
-export const DB_HOST = process.env.DB_HOST
