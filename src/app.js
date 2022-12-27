7abbd3bc16ce614c79baf55cd17f2171365f3a78
Mauro Asselborn <mauasselborn@gmail.com>
2022-12-26 23:47:29 -0300

    Finalizado los controller de productos (nivel basico)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import rutaProductos from './routes/productos.routes.js';
-import { Producto } from './models/Producto.js';
+import express from 'express'
+import rutaProductos from './routes/productos.routes.js'
+import { Producto } from './models/Producto.js'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 //Uso de rutas
-app.use(rutaProductos);
+app.use(rutaProductos)
 
 app.use((req, res) => {
-	res.status(404).json({ message: 'No se encuentra la pagina' });
-});
+    res.status(404).json({ message: 'No se encuentra la pagina' })
+})
 
-export default app;
+export default app
--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize'
 
 export const conexion = new Sequelize('f5ldr', 'root', 'root', {
-	dialect: 'mysql',
-	port: 3308,
-	host: 'localhost',
-});
+    dialect: 'mysql',
+    port: 3306,
+    host: 'localhost',
+})
