df21aa59da085d3bad36266e8ca68900a8c86ac4
ezequielpato <ezequiel.salomon@gmail.com>
2022-12-28 12:08:30 -0300

    Restructuración de codigo en modelo Producto

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -1,41 +1,40 @@
 import { DataTypes } from 'sequelize'
 import { conexion } from '../database.js'
 
-export const Producto = conexion.define(
-    'Producto',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        nombre: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        medida: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0,
-        },
-        valor: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0,
-        },
-        stock: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0,
-        },
-        stock_minimo: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0,
-        },
+const alias = 'Producto'
+
+const columnas = {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
     },
-    {
-        timestamps: true,
-    }
-)
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    valor: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    stock_minimo: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+}
+
+const configuracion = {
+    timestamps: true,
+}
+
+export const Producto = conexion.define(alias, columnas, configuracion)
 
 // TODO: crear relacion marca_id
 // TODO: crear relacion categoria_id
+// TODO: crear relacion medida (litros)
 
 // Producto.sync()
